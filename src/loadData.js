487bc79bf726c6f0b719877732116006e52106b6
Maruti Gollapudi <maruti.gollapudi@gmail.com>
2020-01-13 08:46:18 +0530

    updated

--- src/loadData.js
+++ src/loadData.js
@@ -2,17 +2,22 @@ const firebase = require("firebase");
 // Required for side-effects
 require("firebase/firestore");
 
-import data from './data/sample';
-const collectionName = 'sampleLoad';
+
+const csvToJson = require('convert-csv-to-json');
+
+// import data from './data/sample';
+const collectionName = 'sampleLoad1'; // CHANGE this
 
 // Initialize Cloud Firestore through Firebase
 firebase.initializeApp({
-
+    
   });
 
 var db = firebase.firestore();
 
 
+const data = csvToJson.fieldDelimiter(',').getJsonFromCsv("c:/code/test/testCSV.csv");
+console.log(data);
 
 data.forEach(function(obj) {
     db.collection(collectionName).add(
